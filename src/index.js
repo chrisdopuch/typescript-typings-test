b98c8a95c34d39babe278c203179c3998c16e906
Chris Dopuch <chris.dopuch@gmail.com>
2018-10-22 20:57:59 -0700

    test and build fix

--- src/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.reverse = function (s) { return s.split('').reduce(function (acc, val) { return [val].concat(acc); }, []).join(''); };
